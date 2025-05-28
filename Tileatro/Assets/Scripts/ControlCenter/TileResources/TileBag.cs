48fdafde8426d0b78b2e3d56ff9af2fc2bb49ba0
Alvin-Tulud <alvintulud@gmail.com>
2025-05-28 02:05:33 -0700

    starting tilebag script organization

--- Tileatro/Assets/Scripts/ControlCenter/TileResources/TileBag.cs
+++ Tileatro/Assets/Scripts/ControlCenter/TileResources/TileBag.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
 
 public class TileBag : MonoBehaviour
 {
+    public TileObj baseTile;
+
+    private List<TileObj> totalBag;
+    private List<TileObj> activeBag;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void createBag()
     {
-        
+
+    }
+
+    void shuffleBag(List<TileObj> bagtoShuffle)
+    {
+
+    }
+
+    public void rerollTiles()
+    {
+
     }
 }
---
+++ Tileatro/Assets/Scripts/Tile/EnumTypes/TileObj.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class TileObj : ScriptableObject
+{
+    public Color tile_Color;
+    public Mat tile_Mat;
+    public Shape tile_Shape;
+}
