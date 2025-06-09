9ae5479d21a25a6ab8e2c52c1723aa2a262a8c0d
joostine <72476229+joostine@users.noreply.github.com>
2025-06-08 22:29:56 -0700

    Test

---
+++ Tileatro/Assets/Scripts/Scroller.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scroller : MonoBehaviour
+{
+    [SerializeField] RawImage img;
+    [SerializeField] float x, y;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        img.uvRect = new Rect(img.uvRect.position + new Vector2(x, y) * Time.deltaTime, img.uvRect.size);
+    }
+}
